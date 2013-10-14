670c3c5d429de263cc895ced5324e9d81f6f9bbf
Eren Guven <eren.gueven@deliveryhero.com>
2013-10-14 12:47:14 +0200

    add RedisBackend and change StackOperatorRedis to use it

--- gachette_web/operator.py
+++ gachette_web/operator.py
@@ -4,6 +4,7 @@ import json
 import redis
 
 from exceptions import *
+from garnison_api.backends import RedisBackend
 
 class StackOperatorRedis(object):
     """
@@ -14,96 +15,81 @@ class StackOperatorRedis(object):
         self.server = redis.Redis(redis_host if redis_host else "127.0.0.1")
 
     def get_last_stack_version(self, domain):
-        key = "%s:last_stack_version" % domain
-        version = self.server.get(key)
-        if version is None:
-            version = "1"
-            self.server.set(key, version)
-        return version
+        return RedisBackend().get_domain(domain)["last_stack_version"]
 
     def set_last_stack_version(self, domain, version):
-        key = "%s:last_stack_version" % domain
-        self.server.set(key, version)
-
-    def default_stack_meta(self, override):
-        defaults = {
-            "created_by": "Devops Engineer",
-            "created_at": datetime.datetime.utcnow(),
-            "stable": False,
-            "deployed_staging_at": "Never",
-            "deployed_live_at": "Never",
-        }
-        # TODO check against extra keys?
-        defaults.update(override)
-        return defaults
+        RedisBackend().update_domain(domain, last_stack_version=version)
 
     def initialize_stack(self, stack, **kwargs):
         """
         Initiate stack in redis. Create all meta information fields.
         """
-        stack_meta_key = "%s:stack:%s:meta" % (stack.domain, stack.version)
-        for key, value in self.default_stack_meta(kwargs).items():
-            self.server.set("%s:%s" % (stack_meta_key, key), value)
+        # Run create & update in pipeline (transaction)
+        with RedisBackend().redis.pipeline() as pipe:
+            RedisBackend().create_stack(stack.domain, stack.version)
+            RedisBackend().update_stack(stack.domain, stack.version, **kwargs)
+            pipe.execute()
 
-    def setup_packages(self, stack, from_stack=None):
+    def setup_packages(self, stack, from_stack=False, use_latest_packages=False):
         """
-        If not given id, use last_stack_version to copy packages.
-
-        1. if old_stack defined - copy all package keys to new stack
-        2. if old_stack not define - get list of packages from domain:available_packages, iterate over them
-            for each one get value from domain:<package_name>:last_version
+        If ``use_latest_packages`` use latest package versions
+        Elif ``from_stack`` copy packages from source stack
         """
+        if use_latest_packages:
+            raise NotImplemented("Not yet available")
+        if not from_stack:
+            return
 
         if from_stack and from_stack.domain != stack.domain:
             errstr = "Stack domains do not match. New: %s Source: %s" % (stack.domain, from_stack.domain)
             raise TypeError(errstr)
 
-        available_packages_json = self.server.get("%s:available_packages" % stack.domain)
-        available_packages = json.loads(available_packages_json) if available_packages_json else []
+        # Paranoid check
+        backend = RedisBackend()
+        assert backend.stack_exists(stack.domain, stack.version), "New stack doesn't exist"
+        assert backend.stack_exists(from_stack.domain, from_stack.version), "Old stack doesn't exist"
+        source_stack = backend.get_stack(from_stack.domain, from_stack.version)
+        backend.update_stack(stack.domain, stack.version, packages=source["packages"])
 
-        stack_package_key = "%s:stack:%s:package" % (stack.domain, stack.version)
+        # available_packages_json = self.server.get("%s:available_packages" % stack.domain)
+        # available_packages = json.loads(available_packages_json) if available_packages_json else []
 
-        for package in available_packages:
-            if from_stack:  # get the version from source stack
-                source_stack_package_key = ("%s:stack:%s:package:%s" %
-                                            (from_stack.domain, from_stack.version, package))
-                pkg = json.loads(self.server.get(source_stack_package_key))
-                version = pkg["version"]
-            else:  # get latest version
-                package_key = "package:%s:last_version" % package
-                version = self.server.get(package_key)
-                if version == None:
-                    raise PackageNotBuiltException("%s has never been built" % package)
-            pkg = {
-                "name": package, "version": version, "file_name": "%s-%s.deb" % (package, version)
-            }
+        # stack_package_key = "%s:stack:%s:package" % (stack.domain, stack.version)
 
-            self.server.set("%s:%s" % (stack_package_key, package), json.dumps(pkg))
+        # for package in available_packages:
+        #     if from_stack:  # get the version from source stack
+        #         source_stack_package_key = ("%s:stack:%s:package:%s" %
+        #                                     (from_stack.domain, from_stack.version, package))
+        #         pkg = json.loads(self.server.get(source_stack_package_key))
+        #         version = pkg["version"]
+        #     else:  # get latest version
+        #         package_key = "package:%s:last_version" % package
+        #         version = self.server.get(package_key)
+        #         if version == None:
+        #             raise PackageNotBuiltException("%s has never been built" % package)
+        #     pkg = {
+        #         "name": package, "version": version, "file_name": "%s-%s.deb" % (package, version)
+        #     }
+
+        #     self.server.set("%s:%s" % (stack_package_key, package), json.dumps(pkg))
 
 
-    def test_stack_exists(self, domain, stack_version):
+    def test_stack_exists(self, domain, stack):
         """
         Check if stack exists in redis.
         """
-        stack_meta_key = "%s:stack:%s:meta*" % (domain, version)
-        keys = self.server.keys(stack_meta_key)
-        return True if keys else False
+        return RedisBackend().stack_exists(domain, stack)
 
-    def add_stack_package(self, stack, package_name, package_version, domain=None):
+    def add_stack_package(self, stack, pkg_name, pkg_version, pkg_filename):
         """
         Registers a built package in the stack.
         """
-        # assert (self.test_stack_exists(domain, stack.version),
-        #         "Stack '%s' does not exist" % stack.version)
         
         pkg = {
-            "name": package_name, "version": package_version,
-            "file_name": "%s-%s.deb" % (package_name, package_version)
+            "name": pkg_name, "version": pkg_version,
+            "filename": pkg_filename
         }
-
-        # add to stack
-        self.server.set("%s:stack:%s:package:%s" % (domain, stack.version, package_name),
-                        json.dumps(pkg))
+        RedisBackend().add_stack_package(stack.domain, stack.version, pkg)
 
     def add_reference_package(self, name, version, file_name):
         """
---
+++ garnison_api/backends.py
@@ -0,0 +1,168 @@
+import datetime
+import imp
+import json
+import os
+import redis
+
+import functools
+
+config = os.environ.get('GACHETTE_SETTINGS', './config.rc')
+dd = imp.new_module(config)
+
+with open(config) as config_file:
+    dd.__file__ = config_file.name
+    exec(compile(config_file.read(), config_file.name, 'exec'), dd.__dict__)
+
+class RedisBackend(object):
+    """
+    Handle redis operations eg. checking, listing, creating...
+    """
+    def __init__(self, redis_host=None):
+        self.redis_host = redis_host if redis_host else dd.REDIS_HOST
+        self.redis = redis.Redis(self.redis_host)
+
+        def _set(f):
+            @functools.wraps(f)
+            def wrapper(*args, **kwargs):
+                # patch redis.set to use pipe if pipe defined in context
+                try:
+                    assert isinstance(pipe, redis.client.Pipeline)
+                    print type(self), "Using redis pipline"
+                    pipe.set(*args, **kwargs)
+                except NameError:
+                    f(*args, **kwargs)
+            return wrapper
+
+        # self.redis.original_set = self.redis.set
+        self.redis.set = _set(self.redis.set)
+
+    @property
+    def domain_defaults(self):
+        return dict(
+            last_stack_version = "",
+            last_deployed_staging = "",
+            last_deployed_live = "",
+            available_packages = [],
+        )
+
+    @property
+    def stack_defaults(self):
+        return dict(
+            meta = {
+                "created_by": "Devops Engineer",
+                "created_at": datetime.datetime.utcnow().isoformat(),
+                "stable": False,
+                "deployed_staging_at": "Never",
+                "deployed_live_at": "Never",
+            },
+            packages = {},
+        )
+
+    @property
+    def build_defaults(self):
+        return dict(
+            created_by = "Devops Engineer",
+            created_at = datetime.datetime.utcnow().isoformat(),
+            branch = "",
+            commit = "",
+        )
+
+    def domain_exists(self, domain):
+        val = self.redis.get("domains:%s" % domain)
+        return True if val else False
+
+    def create_domain(self, domain):
+        if self.domain_exists(domain):
+            raise TypeError("Domain '%s' exists" % domain)
+        self.redis.set("domains:%s" % domain, json.dumps(self.domain_defaults))
+
+    def get_domain(self, domain):
+        domain_ = self.redis.get("domains:%s" % domain)
+        return json.loads(domain_) if domain_ else domain_
+
+    def update_domain(self, domain, **kwargs):
+        for k, v in kwargs.items():
+            assert k in self.domain_defaults, "Key '%s' not allowed" % k
+        assert self.domain_exists, "Domain '%s' does not exist" % domain
+        d = self.get_domain(domain)
+        d.update(kwargs)
+        self.redis.set("domains:%s" % domain, json.dumps(d))
+
+    def list_domains(self):
+        all_keys = map(lambda k: k.split(":")[1], self.redis.keys("domains:*"))
+        return list(set(all_keys))
+
+    def stack_exists(self, domain, stack):
+        val = self.redis.get("domains:%s:stacks:%s" % (domain, stack))
+        return True if val else False
+
+    def create_stack(self, domain, stack):
+        if self.stack_exists(domain, stack):
+            raise TypeError("Stack 'domains:%s:stacks:%s' exists" % (domain, stack))
+        if not self.domain_exists(domain):
+            raise TypeError("Domain '%s' does not exist" % domain)
+        self.redis.set("domains:%s:stacks:%s" % (domain, stack), json.dumps(self.stack_defaults))
+
+    def get_stack(self, domain, stack):
+        stack_ = self.redis.get("domains:%s:stacks:%s" % (domain, stack))
+        return json.loads(stack_) if stack_ else stack_
+
+    def update_stack(self, domain, stack, **kwargs):
+        for k, v in kwargs.items():
+            assert k in self.stack_defaults, "Key '%s' not allowed" % k
+        assert self.stack_exists(domain, stack), ("Stack 'domains:%s:stacks:%s' does not exist" %
+                                                  (domain, stack))
+        s = self.get_stack(domain, stack)
+        s.update(kwargs)
+        self.redis.set("domains:%s:stacks:%s" % (domain, stack), json.dumps(s))
+
+    def list_stacks(self, domain):
+        return map(lambda k: k.split(":")[3],
+                   self.redis.keys("domains:%s:stacks:*" % domain))
+
+    def package_exists(self, package, version):
+        val = self.redis.get("packages:%s:%s" % package)
+        return True if val else False
+
+    def create_package(self, package, pkg_dict):
+        if self.package_exists(package):
+            raise TypeError("Package 'packages:%s' exists" % package)
+        self.redis.set("packages:%s" % package, json.dumps(pkg_dict))
+
+    def get_package(self, package):
+        package_ = self.redis.get("packages:%s" % package)
+        return json.loads(package_) if package_ else package_
+
+    def available_packages(self, domain):
+        if not self.domain_exists(domain):
+            raise TypeError("Domain 'domains:%s' does not exist" % domain)
+        return self.get_domain(domain)["available_packages"]
+
+    def add_stack_package(self, domain, stack, pkg_dict):
+        if not self.stack_exists(domain, stack):
+            raise TypeError("Stack 'domains:%s:stacks:%s' exists" % (domain, stack))
+        s = self.get_stack(domain, stack)
+        s["packages"][pkg_dict["name"]] = pkg_dict
+        self.update_stack(domain, stack, packages=s["packages"])
+
+    def lock_exists(self, type_, name):
+        val = self.redis.get("locks:%s:%s" % (type_, name))
+        return True if val else False
+
+    def create_lock(self, type_, name):
+        if self.lock_exists(type_, name):
+            raise TypeError("Lock '%s:%s' exists" % (type_, name))
+        self.redis.set("locks:%s:%s" % (type_, name), datetime.datetime.utcnow().isoformat())
+
+    def list_locks(self):
+        lock_namespace = set(map(lambda k: k.split(":")[1], self.redis.keys("locks:*")))
+        locks = {}
+        for lock_type in lock_namespace:
+            locks[lock_type] = map(lambda k: k.split(":")[2],
+                                   self.redis.keys("locks:%s:*" % lock_type))
+        return locks
+
+    def delete_lock(self, type_, name):
+        if not self.redis.delete("locks:%s:%s" % (type_, name)):
+            print "Lock: 'locks:%s:%s' not found" % (type_, name)
+            # TODO log inexistent
